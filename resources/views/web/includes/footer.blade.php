42847eff656409997675a1e271e4bf2c5dc57dcc
Tope Olotu <topeolotu75@gmail.com>
2020-08-24 00:17:51 +0100

    change

--- resources/views/web/includes/footer.blade.php
+++ resources/views/web/includes/footer.blade.php
@@ -49,7 +49,7 @@
             </div>
             <div class="footer-grid_section text-center">
                 <div class="footer-title-w3ls mb-3" data-aos="fade-up">
-                    <a href="index.html" class="text-uppercase"><img height="90px" width="90px" src="{{ $web_source }}/images/wusob_logo.png" /> WUSOB</a>
+                    <a class="text-uppercase"><img height="90px" width="90px" src="{{ $web_source }}/images/wusob_logo.png" /> WUSOB</a>
                 </div>
                 <div class="footer-text">
                     <p data-aos="fade-up">WUSOB TECHNOLOGIES is an IT firm that specializes in the provision
@@ -71,86 +71,4 @@
 <div class="cpy-right text-center py-3">
     <p class="copy-w3layouts">Copyright &copy;<script>document.write(new Date().getFullYear());</script> WUSOB TECHNOLOGIES. All rights reserved.</a>
     </p>
-</div>
-
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header text-left">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-
-                <div class="login px-4 mx-auto mw-100">
-                    <h5 class="text-left mb-4">Login Now</h5>
-                    <form action="#" method="post">
-                        <div class="form-group">
-                            <label class="mb-2">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" required="">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div class="form-group">
-                            <label class="mb-2">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="" required="">
-                        </div>
-                        <div class="form-check mb-2">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary submit mb-4">Sign In</button>
-                        <p class="text-left pb-4">
-                            <a href="#" data-toggle="modal" data-target="#exampleModalCenter2"> Don't have an account?</a>
-                        </p>
-                    </form>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="exampleModalCenter2" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header text-left">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="login px-4 mx-auto mw-100">
-                    <h5 class="text-left mb-4">Register Now</h5>
-                    <form action="#" method="post">
-                        <div class="form-group">
-                            <label>First name</label>
-
-                            <input type="text" class="form-control" id="validationDefault01" placeholder="" required="">
-                        </div>
-                        <div class="form-group">
-                            <label>Last name</label>
-                            <input type="text" class="form-control" id="validationDefault02" placeholder="" required="">
-                        </div>
-
-                        <div class="form-group">
-                            <label class="mb-2">Password</label>
-                            <input type="password" class="form-control" id="password1" placeholder="" required="">
-                        </div>
-                        <div class="form-group">
-                            <label>Confirm Password</label>
-                            <input type="password" class="form-control" id="password2" placeholder="" required="">
-                        </div>
-
-                        <button type="submit" class="btn btn-primary submit mb-4">Register</button>
-                        <p class="text-left pb-4">
-                            <a href="#">By clicking Register, I agree to your terms</a>
-                        </p>
-                    </form>
-
-                </div>
-            </div>
-
-        </div>
-    </div>
 </div>

0bc711bde68070e9ff713378973886b47ab71d91
MISHI RAY <42464035+mishiray@users.noreply.github.com>
2020-09-28 10:42:15 +0100

    tawk.to implementation

--- resources/views/web-old/layouts/app1.blade.php
+++ resources/views/web-old/layouts/app1.blade.php
@@ -104,19 +104,6 @@
     <script src="{{ $web_source }}/js/custom.js"></script>
     <script src="{{ $web_source }}/js/jquery-2.2.3.min.js"></script>
     @toastr_js
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/5f719dc3f0e7167d00145015/default';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
-    <!--End of Tawk.to Script-->
 </body>
 
 @toastr_render

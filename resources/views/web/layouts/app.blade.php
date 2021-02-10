4225bab1dc964de6d4e9f7146f09c00e233e63f5
Tope Olotu <topeolotu75@gmail.com>
2021-02-10 21:28:01 +0100

    fb-widget-traffic-header

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -4,10 +4,27 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '860848521426306');
+    fbq('track', 'PageView');
+    </script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=860848521426306&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
     <title>@yield('title')  - WusobTech</title>
     <meta name="description" content="Toward IT solutions, wusob technologies is a full customs services firm. In achieving that, it is our business to adapt quickly to meet our clients diverse needs and demands.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
     <meta property="og:site_name" content="Wusobtech"/> <!-- website name -->
     <meta property="og:site" content="https://wusobtech.com"/> <!-- website link -->
@@ -17,7 +34,7 @@
     <meta property="og:url" content=""/> <!-- where do you want your post to link to -->
     <meta property="og:type" content="article"/>
 
-    
+
     <link rel="icon" type="image/png" sizes="32x32" href="{{ $web_source }}/img/wusob_logo.png">
     <!-- <link rel="manifest" href="site.webmanifest"> -->
     <!-- Place favicon.ico in the root directory -->
